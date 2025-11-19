69c083ad0a58f98839080d6571ddb7e8b08d608c
juan david pineda gomez <davidpinedaaaa@gmail.com>
2025-11-19 17:03:16 -0500

    descripcion nullable

--- database/migrations/2025_11_19_211726_change_descripcion_type_in_productos_table.php
+++ database/migrations/2025_11_19_211726_change_descripcion_type_in_productos_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('productos', function (Blueprint $table) {
-            $table->text('descripcion')->change();
+            $table->text('descripcion')->nullable()->change();
         });
     }
 
